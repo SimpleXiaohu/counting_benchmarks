4a3043222f35f9e5f435e8bd8fd780626c290934
SimpleXiaohu <1048448520@qq.com>
2024-02-27 21:00:45 +0800

    add README and change names of some files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# counting_benchmarks
+# About 
+The repo is used to generate string constraints based on the regexes from the papers and the internet.
+
+# benchmark-cnt-set-automata
+The regexes coming from the OOPSLA'20 paper "**Regex Matching with Counting-Set Automata**".
+
+# benchmark-ca-main
+The regexes coming from the USENIX Security'22 paper "**Counting in Regexes Considered Harmful: Exposing ReDoS Vulnerability of Nonbacktracking Matchers**".
+
+# VTLeeLab-LinguaFranca-FSE19-7a763c4
+The regexes coming from the FSE'19 paper "**Why Aren’t Regular Expressions a Lingua Franca? An Empirical Study on the Re-use and Portability of Regular Expressions**".
+The extracted regexes are stored in `extracted`.
+
+# scripts
+The python scripts used to generate string constraints based on the regexes.
+```
+|-- benchmarks_selected_to_smt2.py # The script to translate the regexes in benchmarks-ca-main/Benchmarks-selected to smt2 files.
+|-- VTEELAb_FSE19_regex_extractor.py # The script to extract the regexes from the VTLeeLab-LinguaFranca-FSE19-7a763c4 dataset.
+|-- runner.py # The multiple threads runner interface.
+|-- utils.py # The utility functions. 
+```
+
+# reg2script
+The tool to translate regexes to string constraints, written in ocmal.
---
+++ scripts/benchmarks_selected_to_smt2.py
@@ -0,0 +1,52 @@
+# Input: benchmarks-ca-main/Benchmarks-selected
+# Output: nested and non-nested directory containing responsible smt2 files. For example, the regex coming from bench1-above.txt will be stored in non-nested/above/bench1/instancexxx.smt2, and the regex coming from bench1-nested-between.txt will be stored in nested/between/bench1/instancexxx.smt2
+
+from runner import RunnerInterface
+from dataclasses import dataclass
+import argparse, os
+
+@dataclass
+class smt2Translator(RunnerInterface):
+  instance_count: int = 0
+
+  def __post_init__(self) -> None:
+    super().__post_init__()
+    self.non_nested_above = os.path.join(self.outdir, "non-nested", "above")
+    self.non_nested_between = os.path.join(self.outdir, "non-nested", "between")
+    self.non_nested_tobig = os.path.join(self.outdir, "non-nested", "toobig")
+    self.nested_above = os.path.join(self.outdir, "nested", "above")
+    self.nested_between = os.path.join(self.outdir, "nested", "between")
+    self.nested_tobig = os.path.join(self.outdir, "nested", "toobig")
+    os.makedirs(self.non_nested_above, exist_ok=True)
+    os.makedirs(self.non_nested_between, exist_ok=True)
+    os.makedirs(self.non_nested_tobig, exist_ok=True)
+    os.makedirs(self.nested_above, exist_ok=True)
+    os.makedirs(self.nested_between, exist_ok=True)
+    os.makedirs(self.nested_tobig, exist_ok=True)
+
+
+  def run_single_instance(self, filename: str):
+    self.instance_count += 1
+    print(f"Processing {filename}, instance count {self.instance_count}")
+
+    filename_without_extension = os.path.splitext(os.path.basename(filename))[0]
+    filename_split = filename_without_extension.split("-")
+    regextype = "nested" if "nested" in filename_split else "non-nested"
+    bounding_range = filename_split[-1] # above, between, toobig
+    benchname = filename_split[0]
+    outdir = os.path.join(self.outdir, regextype, bounding_range, benchname)
+    reg2smt2_cmd = f"{os.path.join(basedir, '../reg2script/src/reg2smt')} {filename}  -inter -o {outdir}"
+    print(reg2smt2_cmd)
+    
+
+argparser = argparse.ArgumentParser(prog=__file__, description="Translate benchmarks from txt to smt2")
+argparser.add_argument("bench")
+basedir = os.path.dirname(__file__)
+default_outdir = os.path.join(basedir, "../benchmarks_ca_main_smt2/")
+print(basedir)
+argparser.add_argument("--outdir", help="Output directory for the translated files", default=default_outdir)
+
+args = argparser.parse_args()
+
+
+smt2Translator(benchdir=args.bench, proccess_num=1, outdir=args.outdir, extension="txt").run()
